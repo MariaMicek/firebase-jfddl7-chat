0baf814306a29613c876edc6cf05f127acd0b9e6
Maria Micek <mmicek16@wp.pl>
2019-04-12 10:09:24 +0200

    authorisation is ready, user can log in with google account

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { database } from './firebaseConfig'
+
 
 class App extends Component {
   render() {
--- src/Auth.js
+++ src/Auth.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react'
+import Button from '@material-ui/core/Button'
+import { auth, googleProvider } from './firebaseConfig'
+
+const styles = {
+    root: {
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+}
 
 class Auth extends Component {
     state = {
         isUserLoggedIn: false,
+    }
+
+    componentDidMount = () => {
+        auth.onAuthStateChanged(
+            user => {
+                if (user) {
+                    this.setState({ isUserLoggedIn: true })
+                } else {
+                    this.setState({ isUserLoggedIn: false })
+                }
+            }
+        )
+    }
+
+    onLogInClick = () => {
+        auth.signInWithPopup(googleProvider)
+            .catch(console.log)
     }
 
     render() {
@@ -10,9 +40,21 @@ class Auth extends Component {
             <div>
                 {
                     this.state.isUserLoggedIn ?
-                    this.pros.children
-                    :
-                    <button></button>
+                        this.props.children
+                        :
+                        <div
+                            style={styles.root}
+                        >
+                            <h1>THE BEST CHAT</h1>
+                            <Button
+                                variant={'contained'}
+                                color={'secondary'}
+                                style={styles.button}
+                                onClick={this.onLogInClick}
+                            >
+                                LOG IN BY GOOGLE
+                            </Button>
+                        </div>
                 }
             </div>
         )
